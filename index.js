bd1dec3b79621c9902b23a7ecb4458bcd0b4347c
mgerasika <mgerasika@gmail.com>
2015-04-04 09:28:16 +0300

    1
    1

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.set("view options", { layout: false });
+app.set('views', __dirname + '/public/views');
+app.use("/res", express.static(__dirname + '/res'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    //response.send('Hello World 2!');
+    response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
